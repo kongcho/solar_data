21a5d557776a69b5608d5d13a3095e52bce63a55
CYK <kongc@uchicago.edu>
2017-11-05 12:25:25 -0600

    can split files to match mast limits

--- parse_stars/parse.py
+++ parse_stars/parse.py
@@ -1,15 +1,21 @@
 import csv
+import os
 
-# Variables
-filename_periods = "sample_periodic.txt"
-filename_nonperiods = "sample_nonperiodic.txt"
+## Variables
+#filename_periods = "sample_periodic.txt"
+#filename_nonperiods = "sample_nonperiodic.txt"
+filename_periods = "Table_Periodic.txt"
+filename_nonperiods = "Table_Non_Periodic.txt"
 list_filenames = [filename_periods, filename_nonperiods]
 
-stellar_param = "sample_table.txt"
+#stellar_param = "sample_table.txt"
+stellar_param = "table4.dat"
 stellar_params_i = [1, 4, 7] #KIC = 0, Teff, logg, Fe/H
 
-# Functions
+#good_kids_filename = "good_kids.txt"
+good_kids_file = "good_kids"
 
+## Functions
 # assume all kids are unique from each file
 def get_kids(list_files):
     kid_periods = []
@@ -20,6 +26,7 @@ def get_kids(list_files):
             for col in reader:
                 if col:
                     kid_periods.append(col[0])
+    print("get_kids done")
     return(kid_periods)
 
 # checks if given kids have data from q1-17 kepler targets
@@ -32,8 +39,44 @@ def get_good_kids(param_filename, list_kids):
             for kids_i in range(len(list_kids)):
                 if curr_param_kid == list_kids[kids_i]:
                     list_good_kids.append(curr_param_kid)
+    print("get_good_kids done")
     return list_good_kids
 
-# Tests
-#print(get_kids(list_filenames))
-print(get_good_kids(stellar_param, get_kids(list_filenames)))
+# prints array to column in file, splits up files by number_kids_per_file
+def array_to_file(arr):
+    arr_i = 0
+    total_kids = len(arr)
+    number_kids_per_file = 2
+    output_files_i = 0
+    number_of_files = total_kids//number_kids_per_file + 1
+    remainder_of_files = total_kids%number_kids_per_file
+    good_kids_filename = good_kids_file + "_" + str(output_files_i) + ".txt"
+
+    if os.path.exists(good_kids_filename):
+        ans = input("File already exists, do you want to proceed for all? (y/n) ")
+        ans = ans.strip().lower()
+        if (ans != "y"):
+            print("Quitting...")
+            return 1
+
+    for output_files_i in range(number_of_files):
+        good_kids_filename = good_kids_file + "_" + str(output_files_i) + ".txt"
+        if (output_files_i == number_of_files - 1):
+            number_kids_per_file = remainder_of_files
+        with open(good_kids_filename, 'w') as write_f:
+            writer = csv.writer(write_f, delimiter=',', lineterminator='\n')
+            for i in range(arr_i, arr_i + number_kids_per_file):
+                writer.writerow([arr[i]])
+            arr_i = arr_i + number_kids_per_file
+
+    print("array_to_file done")
+    return 0
+
+#
+def remove_bright_neighbours(list_kids, kids_kepmag):
+    difference_max = 2.0
+    return 0
+
+## Tests
+#array_to_file(["test","1","thurd", "fourrrr", "fiffff"])
+array_to_file(get_good_kids(stellar_param, get_kids(list_filenames)))
