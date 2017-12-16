50f8d3c35c109caa51aa27ab342a3187ca6497a0
CYK <kongc@uchicago.edu>
2017-12-17 01:50:39 +0700

    looks for 'single' files

--- f3_edit/plot_flux.py
+++ f3_edit/plot_flux.py
@@ -11,25 +11,28 @@ from f3 import photometry
 f3_location = "/home/user/Desktop/astrolab/solar_data/f3_edit/"
 
 file_name = "sample_single"
-files_txt = ["./data/" + x.split('.')[0]
-             for x in os.listdir("./data/") if x.endswith(".txt")]
-if len(files_txt) != 0:
-    file_name = files_txt[0]
+targets_files = [file_name]
+
+targets_files_temp = ["./data/" + x.split('.')[0]
+                      for x in os.listdir("./data/")
+                      if x.endswith(".txt") and "single" in x]
+if len(targets_files) != 0:
+    file_name = targets_files_temp[0]
+    targets_files = targets_files_temp
 
 targets_file = file_name + ".txt"
 output_file = file_name + "_plot.pdf"
 log_file = file_name + ".log"
 
-fmt = ['ko', 'rD', 'b^', 'gs']
+# fmt = ['ko', 'rD', 'b^', 'gs']
 
 # Logging
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 handler = logging.FileHandler(log_file)
-handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 # Functions
 
@@ -43,7 +46,7 @@ def plot_data(targ, do_roll=True, ignore_bright=0, image_region=15):
     plt.subplot2grid((3,3), (0,0), colspan=2, rowspan=3)
     for i in range(4):
         g = np.where(targ.qs == i)[0]
-        plt.errorbar(targ.times[g], targ.obs_flux[g], yerr=targ.flux_uncert[i], fmt=fmt[i])
+        plt.errorbar(targ.times[g], targ.obs_flux[g], yerr=targ.flux_uncert[i], fmt=targ.fmt[i])
 
     plt.xlabel('Time', fontsize=15)
     plt.ylabel('Relative Flux', fontsize=15)
@@ -67,18 +70,17 @@ def run_photometry(targ):
 
     plot_data(target)
 
-
-def plot_targets(targets_file):
-
+def plot_targets(targets_file):    
     with PdfPages(output_file) as pdf:
-        with open(targets_file) as f:
-            reader = csv.reader(f)
-            for count, row in enumerate(reader):
-                target = row[0]
-                run_photometry(target)
-                pdf.savefig()
-                plt.close()
-                logger.info(str(count + 1) + "\t" + target + "\tplot_done")
+        for targets_file in targets_files:
+            with open(targets_file) as f:
+                reader = csv.reader(f)
+                for count, row in enumerate(reader):
+                    target = row[0]
+                    run_photometry(target)
+                    pdf.savefig()
+                    plt.close()
+                    logger.info(str(count + 1) + "\t" + target + "\tplot_done")
     logger.info("plot_all_targets_done")
 
 
